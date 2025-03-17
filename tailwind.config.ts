141c4e5a4a2569394b70b1d4674dc0e22577e5d3
ebonian <52095091+ebonian@users.noreply.github.com>
2025-03-18 02:21:01 +0700

    update css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,17 +2,13 @@ import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         hackathon: {
-          primary: "#FF0909",
-          background: "#171717",
+          primary: "var(--hackathon-primary)",
+          background: "var(--hackathon-background)",
         },
       },
       fontFamily: {
